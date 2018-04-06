683484b9181af432d61344441c9592938b92a9e2
facundo <facundoiguerrero@gmail.com>
2018-04-06 11:39:12 -0300

    test

---
+++ tests/test.js
@@ -0,0 +1,12 @@
+var assert = require('assert');
+var app = require('../app');
+
+describe('aplicacion', function() {
+    describe('add function', function() {
+        it('adds numbers', function () {
+            var result = app.get('/api/users/1');
+            print(result);
+            assert.equal(result, 200);
+        });
+    });
+});
