babf15fff84c3a4c5b3d7a0d5e29493bb3d22a9b
facundo <facundoiguerrero@gmail.com>
2018-04-06 11:46:57 -0300

    test func

--- test/test.js
+++ test/test.js
@@ -2,10 +2,5 @@ var assert = require('assert');
 var app = require('../app');
 
 describe('aplicacion', function() {
-    describe('add function', function() {
-        it('adds numbers', function () {
-            var result = app.get('/api/users/1');
-            assert.equal(result, 200);
-        });
-    });
+    assert(200,200);
 });
