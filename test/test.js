0011bfd19a47ade0cc95fd7b9ea73ca6cea12dbe
facundo <facundoiguerrero@gmail.com>
2018-04-07 10:49:38 -0300

    tests andando, falta hacer muchos mas pero ya anda el framework

--- test/test.js
+++ test/test.js
@@ -1,56 +1,27 @@
 var assert = require('assert');
-var app = require('../app');
+var request = require("request");
+var base_url = "https://picappss.herokuapp.com/"
 
 describe('USER', function() {
            assert(200,200);
 });
 
-/*//During the test the env variable is set to test
-process.env.NODE_ENV = 'test';
-
-var assert = require('assert');
-let user = require('../app/models/user');
-
-let mongoose = require("mongoose");
-//Require the dev-dependencies
-let chai = require('chai');
-let chaiHttp = require('chai-http');
-let server = require('../server');
-let should = chai.should();
-
-chai.use(chaiHttp);
-//Our parent block
-*/
-/*describe('USERS', () => {
-    beforeEach((done) => { //Before each test we empty the database
-        user.remove({}, (err) => {
-            done();
-        });
-    });
-
-    describe('/GET book', () => {
-        it('it should GET all the books', (done) => {
-            chai.request(server)
-                .get('/book')
-                .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body.should.be.a('array');
-                    res.body.length.should.be.eql(0);
-                    done();
-                });
-        });
-    });
-
-});
 describe('USER', function() {
-
     describe('GET', function(){
 
-        it('Busco el usuario con indice 1', function () {
-           app.post('/api/users');
-           var result = app.get('/api/users');
-           console.log(result);
-           assert(result,200);
-       });
+        it('Probamos hacer un get a la pagina principal', function(){
+            request.get(base_url,function (error,response,body) {
+                assert.equal(200,response.statusCode);
+                done();
+            });
+        });
+
+        it("returns Welcome to the beginning of nothingness.", function() {
+            request.get(base_url, function (error, response, body) {
+                //expect(body).toBe("Hello World");
+                assert.equal("Welcome to the beginning of nothingness.", body);
+                done();
+            });
+        });
     });
-});*/
+});
