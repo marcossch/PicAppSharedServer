3b08f53af52ad6548c3043b6b6a31a8f3da658b2
facundo <facundoiguerrero@gmail.com>
2018-04-11 17:24:20 -0300

    un test cualquiera

---
+++ test/test.js
@@ -0,0 +1,11 @@
+var assert = require('assert');
+const sinon = require('sinon');
+const chai = require('chai');
+const request = require('request');
+const should = chai.should();
+
+describe('User', function() {
+    it('CreateUser', function() {
+	assert(200 == 200);
+	});
+});
