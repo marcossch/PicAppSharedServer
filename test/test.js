1f3530a1a22f2367f6904e3f4c8d09e276b1058d
facundo <facundoiguerrero@gmail.com>
2018-04-12 00:33:51 -0300

    ALFIN ANDAN LOS FUCKINGS TEST

--- test/test.js
+++ test/test.js
@@ -1,11 +1,30 @@
-var assert = require('assert');
-const sinon = require('sinon');
-const chai = require('chai');
-const request = require('request');
-const should = chai.should();
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../app');
+var should = chai.should();
 
-describe('User', function() {
-    it('CreateUser', function() {
-	assert(200 == 200);
-	});
+chai.use(chaiHttp);
+
+
+describe('Get a la pagina principal', function() {
+
+    it('Check Status 200', function(done) {
+        chai.request(server)
+            .get('/')
+            .end(function(err, res){
+                res.should.have.status(200);
+                done();
+            });
+    });
+
+	it('Check Return Welcome to PicApp web services.', function(done) {
+        chai.request(server)
+            .get('/')
+            .end(function(err, res){
+                res.body.message.should.equal('Welcome to PicApp web services.');
+                done();
+            });
+    });
+
+
 });
