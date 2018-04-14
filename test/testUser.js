9008b12126432c956989deb298a9211c225dfaa5
facundo <facundoiguerrero@gmail.com>
2018-04-14 14:56:40 -0300

    veo pruebas

--- test/testUser.js
+++ test/testUser.js
@@ -55,7 +55,7 @@ describe('Test para User', function() {
         });
 
 
-        /*it('Check Status 401: unauthorized', function(done) {
+        it('Check Status 401: unauthorized', function(done) {
             chai.request(server)
                 .post('/api/user/?ApplicationToken=tok')
                 .set('content-type', 'application/x-www-form-urlencoded')
@@ -64,7 +64,7 @@ describe('Test para User', function() {
                     res.should.have.status(401);
                     done();
                 });
-            });*/
+            });
 
         /*it('Test Integracion: Check status 200 and Type Data', function(done) {
             chai.request(server)
