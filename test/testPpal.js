dfdf6c36c7e58027c2e5bcd5c20158e2e2b46f7c
facundo <facundoiguerrero@gmail.com>
2018-04-15 18:57:41 -0300

    a ver travis del orto

--- test/testPpal.js
+++ test/testPpal.js
@@ -27,6 +27,16 @@ describe('Pagina Principal', function() {
                 done();
             });
         });
+
+    it('Post Superuser', (done) => {
+        chai.request(server)
+            .post('/api/user/super')
+            .send({username: 'superuser', password: 'steelsoft', id: 0, _rev: 'asd', applicationOwner: 'grupo3'})
+            .end((err, res) => {
+                console.log(res);
+                res.should.have.status(200);
+                done();
+            });
 });
 
 
