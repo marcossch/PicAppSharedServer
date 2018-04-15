adf4ff68f37f5ee51fac68dafe5da73cb28b002f
facundo <facundoiguerrero@gmail.com>
2018-04-15 19:29:32 -0300

    todo

--- test/testUser.js
+++ test/testUser.js
@@ -1,5 +1,23 @@
 process.env.NODE_ENV = 'test';
 
+var nock = require('nock');
+var request = require('supertest')("http://127.0.0.1:3000");
+var expect = require('chai').expect;
+
+describe("Testing User", function () {
+
+    it("Get all users",function (done) {
+        nock("http://127.0.0.1:3000").get('/api/users').reply(200,{"status": 200, "message":"ok"});
+
+        request.get('/api/users').end(function(err,res){
+            expect(res.body.status).to.equal(200);
+            done();
+        });
+    });
+});
+
+
+/*
 //let User = require('../server/controllers/users');
 //Require the dev-dependencies
 let chai = require('chai');
@@ -9,19 +27,19 @@ let server = require('../app');
 describe("Testing User", function () {
 
 
-    /*it('Post Superuser', (done) => {
+    it('Post Superuser', (done) => {
         chai.request(server)
             .post('/api/user/super')
             .send({username: 'superuser', password: 'steelsoft', id: 0, _rev: 'asd', applicationOwner: 'grupo3'})
             .end((err, res) => {
-                console.log(res);
+
                 res.should.have.status(200);
                 done();
             });
-        });*/
+    });
 
 
-    /*it('Get by Username: Superuser', (done) => {
+    it('Get by Username: Superuser', (done) => {
         chai.request(server)
             .get('/api/users/superuser')
             .end((err, res) => {
@@ -37,22 +55,5 @@ describe("Testing User", function () {
                 res.should.have.status(204);
                 done();
             });
-        });*/
-});
-
-
-
-
-/*
-superagent
-
-        request
-            .post('127.0.0.1:3000/api/user/super')
-            .send({username: 'superuser', password: 'steelsoft', id: 0, _rev: 'asd', applicationOwner: 'grupo3'})
-            .set('X-API-Key', 'foobar')
-            .set('accept', 'json')
-            .end((err, res) => {
-                console.log(res);
-                done();
-            });
- */
+        });
+});*/
