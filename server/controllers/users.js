ab28d8302ddd948c599b0ba981ed039a5900e8ad
facundo <facundoiguerrero@gmail.com>
2018-05-17 16:41:09 -0300

    a ver esta porongaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -193,51 +193,45 @@ module.exports = {
                             "(parámetros faltantes) o validación fallida", error})});
     },
 
-    /* PRIVADO PARA PRUEBAS  */
+    /*PRIVADO PARA PRUEBAS*/
 
     supercreate: function (req, res) {
 
-
-            /*if (req.query.key != "steelsoft") {
-                return res.status(401).send({
-                    code: '401',
-                    message: 'Unauthorized',
-                    aux: req.query.key
-                });
-            }
-            else {*/
-
-                //si el ApiKey se valido entonces se crea el usuario
-
-                return User.create({
-                    name: req.body.username,
-                    password: req.body.password,
-                    id: req.body.id,
-                    ref: req.body._rev,
-                    applicationOwner: req.body.applicationOwner,
+        return User.create({
+            name: req.body.username,
+            password: req.body.password,
+            id: req.body.id,
+            ref: req.body._rev,
+            applicationOwner: req.body.applicationOwner,
+        })
+            .then(user => res.status(200).json({
+                metadata: {
+                    version: "1.0"
+                },
+                user: {
+                    id: user.id,
+                    _rev: user.ref,
+                    applicationOwner: user.applicationOwner,
+                    username: user.name
+                }
+            }))
+            .catch(error => {
+                res.status(400).send({
+                    code: '400',
+                    message: "Incumplimiento de precondiciones " +
+                    "(parámetros faltantes) o validación fallida", error
                 })
-                    .then(user => res.status(200).json({
-                        metadata: {
-                            version: "1.0"
-                        },
-                        user: {
-                            id: user.id,
-                            _rev: user.ref,
-                            applicationOwner: user.applicationOwner,
-                            username: user.name
-                        }
-                    }))
-                    .catch(error => {
-                        res.status(400).send({
-                            code: '400',
-                            message: "Incumplimiento de precondiciones " +
-                            "(parámetros faltantes) o validación fallida", error
-                        })
-                    });
+            });
+    },
 
+    listAux: function(req) { //devuelve todos los usuarios
+        let users = User.findAll();
+            if(users){
+              return {code: 200,
+                      res: users};
             }
-
-    //},
+            return {code:500};
+    }
 
 };
 
--- test/userTest.js
+++ test/userTest.js
@@ -1,22 +1,21 @@
 'use strict';
-process.env.NODE_ENV = 'test';
-/*Imports Generales*/
-var chai = require('chai');
-var chaiHttp = require('chai-http');
-var should = chai.should();
-var expect = require('chai').expect;
-chai.use(chaiHttp);
-var sinon = require('sinon');
-var mocks = require('node-mocks-http');
-var assert = chai.assert;
-
-/*Import User*/
-var userGetId = require('../server/controllers/users').retAux;
 var user = require('../server/controllers/users.js');
-
+const expect = require('chai').expect;
 
 describe('Test Module: User', () => {
 
+  it('Obtengo todos los usuarios Status=200', () => {
+    var response = user.listAux();
+    console.log(response.code);
+    expect(response.code).to.be.equal(200);
+  })
+  //var a = user.retAux();
+  //console.log('Expect: 400, and obtain: '+a );
+  //if(a == 400){
+    //console.log("PASSOOOOOO");
+    //return true;
+  //}
+  //return false;
   /*it('Obtengo usuario by username, StatusCode = 404', () => {
       var req = {params:{username:'facu'}};
       var a = user.retrieve(req);
