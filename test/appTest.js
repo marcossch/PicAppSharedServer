00210c259d1250f9644fcda3e5bdb0f72b0fa307
facundo <facundoiguerrero@gmail.com>
2018-06-06 16:35:48 -0300

    otra libreria

--- test/appTest.js
+++ test/appTest.js
@@ -5,31 +5,20 @@ var chai = require('chai');
 var chaiHttp = require('chai-http');
 var server = require('../app');
 var should = chai.should();
+var request = require('supertest');
 
 chai.use(chaiHttp);
 
 
 describe('Pagina Principal', function() {
     it('Check Status 200', function (done) {
-        chai.request(server)
-            .get('/')
-            .end(function (err, res) {
-                res.should.have.status(200);
-                done();
-            });
-        });
-
-    it('Check Return Welcome to PicApp web services.', function (done) {
-        chai.request(server)
-            .get('/')
-            .end(function (err, res) {
-                res.body.message.should.equal('Welcome to PicApp web services.');
-                done();
-            });
-        });
+      request(server).get('/')
+        .expect(200)
+        .expect(/Welcome to PicApp web services./, done)
+    });
 
     // it('Check Status 200', function (done) {
-    //     chai.request(server)
+    //     request(server)
     //     .post('/api/user/super')
     //     .set('content-type', 'application/json')
     //     .send({"username": "superuser",
